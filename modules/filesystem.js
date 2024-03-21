b68f61dd11f98570b788ec3424290eee35f7fbc0
tiff <github@0x8c.org>
2024-03-20 20:01:15 -0400

    Initial commit

---
+++ modules/filesystem.js
@@ -0,0 +1,13 @@
+function mockDirs() {
+  return {
+    root: "/Users/tiff",
+    currentWorkingDirectory: "/Users/tiff/Sites/khajiit",
+    currentDirectories: ["styles", "scripts", "images", "resume"],
+    currentFiles: ["index.html"],
+    fileContents: {
+      "index.html": "Hello World",
+    },
+  };
+}
+
+export default mockDirs;
